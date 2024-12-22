271fc59cfba61f2470f961d89bbfa6af8c603f34
Sahil Rajwar <sahilrajwar2004@gmail.com>
2024-12-23 02:43:57 +0530

    setup.py updated

--- setup.py
+++ setup.py
@@ -3,15 +3,14 @@ from pybind11.setup_helpers import Pybind11Extension, build_ext
 import sys
 import os
 
-# Get Python include directory properly
 python_include_dir = os.path.join(sys.prefix, 'include', f'python{sys.version_info.major}.{sys.version_info.minor}')
 
 ext_modules = [
   Pybind11Extension(
-    "pynum.dtypes",  # Module name
-    ["pynum/src/dtypes.cpp"],  # Source file
-    include_dirs=[python_include_dir],  # Properly specify include directory
-    extra_compile_args=['-std=c++17'],  # C++17 support
+    "pynum.dtypes",
+    ["pynum/src/dtypes.cpp"],
+    include_dirs=[python_include_dir],
+    extra_compile_args=['-std=c++17'],
     language='c++'
   ),
 ]
@@ -21,12 +20,15 @@ pkg_name = {
 }
 
 setup(
-  name="pynum",
-  version="0.0.1",
-  packages=find_packages(),
-  ext_modules=ext_modules,
-  cmdclass={"build_ext": build_ext},
-  package_data=pkg_name,
-  author="Sahil Rajwar",
-  python_requires='>=3.10',  # Added Python version requirement
+  name = "pynum",
+  version = "0.0.1",
+  url = "https://github.com/0xhilSa/pynum",
+  packages = find_packages(),
+  ext_modules = ext_modules,
+  cmdclass = {"build_ext": build_ext},
+  package_data = pkg_name,
+  author = "Sahil Rajwar",
+  long_description_content_type = "text/markdown",
+  python_requires = ">=3.10",
+  description = "a small python library for 1D and 2D arrays with GPU supports (WIP)"
 )
