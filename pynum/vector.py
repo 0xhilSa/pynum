bb078fe885051f77d776b2cfccbd734974b4034e
Sahil Rajwar <sahilrajwar2004@gmail.com>
2024-12-27 01:33:13 +0530

    vector.py updated

--- pynum/vector.py
+++ pynum/vector.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass
 import random
 from . import dtypes
 
+
 SUPPORTED_DEVICES = {"cpu"}
 
 @dataclass(frozen = True)
@@ -16,7 +17,7 @@ class Vector:
   def __init__(self, object:List[Any], dtype:Type=None, const:bool=False, device:str="cpu"):
     if device not in SUPPORTED_DEVICES: raise RuntimeError(f"Unsupported device '{device}'. Supported devices: {', '.join(SUPPORTED_DEVICES)}")
     if not object: raise ValueError("The input list cannot be empty")
-    self.__object, self.__dtype  = self.__check__(object, dtype)
+    self.__object, self.__dtype  = self.__check__(object,dtype)
     self.__device = device
     self.__shape = Shape((len(object),))
     self.__const = const
@@ -27,7 +28,7 @@ class Vector:
     elif all(isinstance(x,(int,dtypes.int8,dtypes.int16,dtypes.int32,dtypes.int64))for x in object): dtype = dtypes.int64
     else: dtype = dtypes.float64
     return [dtype(x) for x in object],dtype
-  def __repr__(self): return f"<Vector(size={self.__shape}, dtype={self.__dtype.__name__}, device={self.__device}, device={self.__device})>"
+  def __repr__(self): return f"<Vector(size={self.__shape}, dtype={self.__dtype.__name__}, const={self.__const}, device={self.__device})>"
   def __getitem__(self, index:Union[int,slice]):
     if isinstance(index,slice):
       sliced_obj = self.__object[index]
@@ -43,5 +44,17 @@ class Vector:
   @property
   def shape(self): return self.__shape
   def is_const(self): return self.__const
-  def rand(self, length:int, seed:Optional[int]=None):
-    pass
+  @staticmethod
+  def rand(length:int, dtype:Type=dtypes.float64, lower_bound:int=0, upper_bound:int=1, seed:Optional[int]=None, const:bool=False, device="cpu"):
+    if seed is not None: random.seed(seed)
+    if dtype in (float,dtypes.float32,dtypes.float64): rand_vec = [random.uniform(lower_bound,upper_bound) for _ in range(length)]
+    elif dtype in (int,dtypes.int8,dtypes.int16,dtypes.int32,dtypes.int64): rand_vec = [random.uniform(lower_bound,upper_bound) for _ in range(length)]
+    else: TypeError(f"Random generation not supported for dtype {self.__dtype.__name__}")
+    return Vector(rand_vec, dtype=dtype, const=const, device=device)
+  @staticmethod
+  def zeros(length:int, dtype:Type=dtypes.float64, const:bool=False, device="cpu"): return Vector([0] * length, dtype=dtype, const=const, device=device)
+  @staticmethod
+  def ones(length:int, dtype:Type=dtypes.float64, const:bool=False, device="cpu"): return Vector([1] * length, dtype=dtype, const=const, device=device)
+  @staticmethod
+  def fill(value:Any, length:int, dtype:Type=dtypes.float64, const:bool=False, device="cpu"): return Vector([value] * length, dtype=dtype, const=const, device=device)
+
