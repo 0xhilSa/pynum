8df5e7e1d09b297fae832afaefb96abee70891d0
Sahil Rajwar <sahilrajwar2004@gmail.com>
2025-01-10 02:23:11 +0530

    cuda.py update

--- pynum/cuda.py
+++ pynum/cuda.py
@@ -1,4 +1,48 @@
-from .src.cuda_stream import *
+# allocators for different data types
+from .src.cuda_stream import cuda_alloc_int
+from .src.cuda_stream import cuda_alloc_long
+from .src.cuda_stream import cuda_alloc_double
+from .src.cuda_stream import cuda_alloc_complex
+
+# copy from host to device
+from .src.cuda_stream import memcpy_htod_int
+from .src.cuda_stream import memcpy_htod_long
+from .src.cuda_stream import memcpy_htod_double
+from .src.cuda_stream import memcpy_htod_complex
+
+# copy from device to host
+from .src.cuda_stream import memcpy_dtoh_int
+from .src.cuda_stream import memcpy_dtoh_long
+from .src.cuda_stream import memcpy_dtoh_double
+from .src.cuda_stream import memcpy_dtoh_complex
+
+# getters(index)
+from .src.cuda_stream import get_value_int
+from .src.cuda_stream import get_value_long
+from .src.cuda_stream import get_value_double
+from .src.cuda_stream import get_value_complex
+
+# getter(slice)
+from .src.cuda_stream import get_slice_int
+from .src.cuda_stream import get_slice_long
+from .src.cuda_stream import get_slice_double
+from .src.cuda_stream import get_slice_complex
+
+# setters
+from .src.cuda_stream import set_value_int
+from .src.cuda_stream import set_value_long
+from .src.cuda_stream import set_value_double
+from .src.cuda_stream import set_value_complex
+
+from .src.cuda_stream import set_slice_int
+from .src.cuda_stream import set_slice_long
+#from .src.cuda_stream import set_slice_double
+#from .src.cuda_stream import set_slice_complex
+
+# free-bies
+from .src.cuda_stream import cuda_free
+from .src.cuda_stream import cuda_query_free_memory
+
 import subprocess
 
 def is_cuda_available():
