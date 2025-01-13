249dfdf3556dc47d4ec1117ab454ceaec62a4df5
Sahil Rajwar <sahilrajwar2004@gmail.com>
2025-01-14 01:24:18 +0530

    cuda.py updated

--- pynum/cuda.py
+++ pynum/cuda.py
@@ -4,50 +4,50 @@ defined at './pynum/src/cuda_stream.c'
 """
 
 # allocators for different data types
-from .src.cuda_stream import cuda_alloc_int
-from .src.cuda_stream import cuda_alloc_long
-from .src.cuda_stream import cuda_alloc_double
-from .src.cuda_stream import cuda_alloc_complex
+from .src.cuda_stream import alloc_short
+from .src.cuda_stream import alloc_int
+from .src.cuda_stream import alloc_long
+from .src.cuda_stream import alloc_double
+from .src.cuda_stream import alloc_complex
 
 # copy from host to device
+from .src.cuda_stream import memcpy_htod_short
 from .src.cuda_stream import memcpy_htod_int
 from .src.cuda_stream import memcpy_htod_long
 from .src.cuda_stream import memcpy_htod_double
 from .src.cuda_stream import memcpy_htod_complex
 
 # copy from device to host
+from .src.cuda_stream import memcpy_dtoh_short
 from .src.cuda_stream import memcpy_dtoh_int
 from .src.cuda_stream import memcpy_dtoh_long
 from .src.cuda_stream import memcpy_dtoh_double
 from .src.cuda_stream import memcpy_dtoh_complex
 
 # getters(index)
+from .src.cuda_stream import get_value_short
 from .src.cuda_stream import get_value_int
 from .src.cuda_stream import get_value_long
 from .src.cuda_stream import get_value_double
 from .src.cuda_stream import get_value_complex
 
 # getter(slice)
+from .src.cuda_stream import get_slice_short
 from .src.cuda_stream import get_slice_int
 from .src.cuda_stream import get_slice_long
 from .src.cuda_stream import get_slice_double
 from .src.cuda_stream import get_slice_complex
 
 # setters(index)
+from .src.cuda_stream import set_value_short
 from .src.cuda_stream import set_value_int
 from .src.cuda_stream import set_value_long
 from .src.cuda_stream import set_value_double
 from .src.cuda_stream import set_value_complex
 
-# setters(slice)
-from .src.cuda_stream import set_slice_int
-from .src.cuda_stream import set_slice_long
-#from .src.cuda_stream import set_slice_double
-#from .src.cuda_stream import set_slice_complex
-
-# free-bies
-from .src.cuda_stream import cuda_free
-from .src.cuda_stream import cuda_query_free_memory
+# freebie
+from .src.cuda_stream import free
+from .src.cuda_stream import query_free_memory
 
 import subprocess
 
