33125c731246cd770a761c863307f2f0cc1673b5
Sahil Rajwar <sahilrajwar2004@gmail.com>
2025-01-16 23:47:36 +0530

    exmaples

---
+++ examples/vec.py
@@ -0,0 +1,13 @@
+from pynum.vector import Vector
+
+x = Vector([1+3j,2-5j,-0.7+12j,-0.9-1j], dtype=complex, device="CUDA")
+
+print(x)
+print(x.numpy())
+print(x.device)
+
+x.cpu()   # vector from CUDA to CPU
+
+print(x)
+print(x.numpy())
+print(x.device)
