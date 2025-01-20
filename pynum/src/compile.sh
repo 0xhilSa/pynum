741a545c9c8974ac4d86ab11573f1ced14d5c2af
Sahil Rajwar <sahilrajwar2004@gmail.com>
2025-01-20 23:16:18 +0530

    compile.sh updated

--- pynum/src/compile.sh
+++ pynum/src/compile.sh
@@ -1,4 +1,4 @@
 nvcc --compiler-options '-fPIC' -std=c++17 -shared \
   $(python3 -m pybind11 --includes) \
-  cuda_stream.cpp -o cuda_stream$(python3-config --extension-suffix) \
+  cuda_stream.cu -o cuda_stream$(python3-config --extension-suffix) \
   -lcudart
