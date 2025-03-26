b64a10c73bab4a97e6f4209121f9d7920afa2242
Sahil Rajwar <justsahilrajwar2004@gmail.com>
2025-03-27 01:40:12 +0530

    setters were added for both 'cuda' and 'cpu' devices

--- pynum/csrc/pycu.cu
+++ pynum/csrc/pycu.cu
@@ -3,6 +3,7 @@
 #include <cuda_runtime.h>
 #include <cuComplex.h>
 #include <python3.10/floatobject.h>
+#include <python3.10/modsupport.h>
 #include <python3.10/object.h>
 #include <python3.10/pyerrors.h>
 #include <python3.10/pyport.h>
@@ -221,6 +222,90 @@ static PyObject* setitem_index(PyObject* self, PyObject* args){
   Py_RETURN_NONE;
 }
 
+static PyObject* setitem_slice(PyObject* self, PyObject* args){
+  PyObject *capsule, *values;
+  Py_ssize_t length, start, stop, step;
+  const char* fmt;
+
+  if(!PyArg_ParseTuple(args, "OOnnnns", &capsule, &values, &length, &start, &stop, &step, &fmt)) return NULL;
+
+  void* device_ptr = PyCapsule_GetPointer(capsule, "cuda_memory");
+  if(!device_ptr){
+    PyErr_SetString(PyExc_ValueError, "Invalid memory capsule");
+    return NULL;
+  }
+
+  size_t size = get_type_size(fmt);
+  if(size == 0) return NULL;
+
+  if(start < 0 || stop <= start || step <= 0) {
+    PyErr_SetString(PyExc_ValueError, "Invalid slice indices");
+    return NULL;
+  }
+
+  Py_ssize_t num_elements = (stop - start + step - 1) / step;
+
+  if(!PySequence_Check(values) || PySequence_Length(values) != num_elements){
+    PyErr_SetString(PyExc_ValueError, "Slice values must match slice length");
+    return NULL;
+  }
+
+  void* host_values = malloc(num_elements * size);
+  if(!host_values){
+    PyErr_SetString(PyExc_MemoryError, "Failed to allocate host memory");
+    return NULL;
+  }
+
+  for(Py_ssize_t i = 0; i < num_elements; i++){
+    PyObject* value = PySequence_GetItem(values, i);
+    void* elem_ptr = (char*)host_values + i * size;
+
+    if(strcmp(fmt, "?") == 0)      *(bool*)elem_ptr = PyObject_IsTrue(value);
+    else if(strcmp(fmt, "b") == 0) *(char*)elem_ptr = (char)PyLong_AsLong(value);
+    else if(strcmp(fmt, "B") == 0) *(unsigned char*)elem_ptr = (unsigned char)PyLong_AsUnsignedLong(value);
+    else if(strcmp(fmt, "h") == 0) *(short*)elem_ptr = (short)PyLong_AsLong(value);
+    else if(strcmp(fmt, "H") == 0) *(unsigned short*)elem_ptr = (unsigned short)PyLong_AsUnsignedLong(value);
+    else if(strcmp(fmt, "i") == 0) *(int*)elem_ptr = (int)PyLong_AsLong(value);
+    else if(strcmp(fmt, "I") == 0) *(unsigned int*)elem_ptr = (unsigned int)PyLong_AsUnsignedLong(value);
+    else if(strcmp(fmt, "l") == 0) *(long*)elem_ptr = PyLong_AsLong(value);
+    else if(strcmp(fmt, "L") == 0) *(unsigned long*)elem_ptr = PyLong_AsUnsignedLong(value);
+    else if(strcmp(fmt, "q") == 0) *(long long*)elem_ptr = PyLong_AsLongLong(value);
+    else if(strcmp(fmt, "Q") == 0) *(unsigned long long*)elem_ptr = PyLong_AsUnsignedLongLong(value);
+    else if(strcmp(fmt, "f") == 0) *(float*)elem_ptr = (float)PyFloat_AsDouble(value);
+    else if(strcmp(fmt, "d") == 0) *(double*)elem_ptr = PyFloat_AsDouble(value);
+    else if(strcmp(fmt, "g") == 0) *(long double*)elem_ptr = (long double)PyFloat_AsDouble(value);
+    else if(strcmp(fmt, "F") == 0){
+      Py_complex pyc = PyComplex_AsCComplex(value);
+      ((cuFloatComplex*)elem_ptr)->x = (float)pyc.real;
+      ((cuFloatComplex*)elem_ptr)->y = (float)pyc.imag;
+    }
+    else if(strcmp(fmt, "D") == 0){
+      Py_complex pyc = PyComplex_AsCComplex(value);
+      ((cuDoubleComplex*)elem_ptr)->x = pyc.real;
+      ((cuDoubleComplex*)elem_ptr)->y = pyc.imag;
+    }
+    else {
+      free(host_values);
+      PyErr_SetString(PyExc_TypeError, "Unsupported data type for slice assignment");
+      return NULL;
+    }
+
+    Py_DECREF(value);
+  }
+
+  for(Py_ssize_t i = 0; i < num_elements; i++){
+    Py_ssize_t device_index = start + i * step;
+    CUDA_ERROR(cudaMemcpy(
+      (char*)device_ptr + device_index * size, 
+      (char*)host_values + i * size, 
+      size, 
+      cudaMemcpyHostToDevice
+    ));
+  }
+
+  free(host_values);
+  Py_RETURN_NONE;
+}
 
 static PyMethodDef methods[] = {
   {"toCuda", toCuda, METH_VARARGS, "Move host memory to CUDA device memory"},
@@ -228,6 +313,7 @@ static PyMethodDef methods[] = {
   {"getitem_index", getitem_index, METH_VARARGS, "Get value via index"},
   {"getitem_slice", getitem_slice, METH_VARARGS, "Get value via slice"},
   {"setitem_index", setitem_index, METH_VARARGS, "set item through index"},
+  {"setitem_slice", setitem_slice, METH_VARARGS, "set item through slice"},
   {NULL, NULL, 0, NULL}
 };
 
