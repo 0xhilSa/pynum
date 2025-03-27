f421d64850f3e0902cf6e2187a64c3027ee77fa7
Sahil Rajwar <justsahilrajwar2004@gmail.com>
2025-03-28 00:10:20 +0530

    minor changes

--- setup.py
+++ setup.py
@@ -12,7 +12,6 @@ setup(
   package_data={"pynum.csrc": ["*.so", "*.pyi"]},
   classifiers=[
     "Programming Language :: Python :: 3",
-    "License :: OSI Approved :: MIT License",
     "Operating System :: OS Independent",
   ],
   python_requires=">=3.6",
